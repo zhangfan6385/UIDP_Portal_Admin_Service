6481b9304b55b2de3e3059292591d45eed4211fe
zhangshe <1312719913@qq.com>
2018-11-20 10:18:32 +0800

    增加usertype

--- STORE.WebAPI.Admin/Controllers/UserController.cs
+++ STORE.WebAPI.Admin/Controllers/UserController.cs
@@ -250,7 +250,8 @@ namespace STORE.WebAPI.Controllers
                         userSex = 0,
                         departCode = "",
                         departName = "",
-                        departId = ""
+                        departId = "",
+                        userType = "0"
                     });
                 }
                 //string token = UIDP.UTILITY.AccessTokenTool.GetAccessToken(d["userId"].ToString());
@@ -266,6 +267,7 @@ namespace STORE.WebAPI.Controllers
                     string _deptCode = dt.Rows[0]["ORG_CODE"] == null ? "" : dt.Rows[0]["ORG_CODE"].ToString();
                     string _deptName = dt.Rows[0]["ORG_SHORT_NAME"] == null ? "" : dt.Rows[0]["ORG_SHORT_NAME"].ToString();
                     string _deptId = dt.Rows[0]["ORG_ID"] == null ? "" : dt.Rows[0]["ORG_ID"].ToString();
+                    string _userType = dt.Rows[0]["AUTHENTICATION_TYPE"] == null ? "" : dt.Rows[0]["AUTHENTICATION_TYPE"].ToString();
                     STORE.LOG.SysLog log = new LOG.SysLog();
                     //log.Info(DateTime.Now, d["userId"].ToString(), _name, ClientIp, 0, "info", "",1);
                     log.Info(DateTime.Now, userID, _name, ClientIp, 0, "info", "", 1);
@@ -285,7 +287,8 @@ namespace STORE.WebAPI.Controllers
                         userSex = _userSex,
                         departCode = _deptCode,
                         departName = _deptName,
-                        departId = _deptId
+                        departId = _deptId,
+                        userType = _userType
                     });
                 }
                 return Json(new
@@ -304,7 +307,8 @@ namespace STORE.WebAPI.Controllers
                     userSex = 0,
                     departCode = "",
                     departName = "",
-                    departId=""
+                    departId="",
+                    userType = "0"
                 });
             }
             catch (Exception ex)
